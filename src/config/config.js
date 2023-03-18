de3ec07312d208a05806634575c213ad7c4a5718
jmhalire <jmhalire@gmail.com>
2023-03-18 11:59:17 -0500

    actualizando archivo config

---
+++ src/config/config.js
@@ -0,0 +1,5 @@
+const config = {
+    JWTSECRET: "mysecret",
+    MONGODB_URI: "mongodb+srv://jaimehalire:17e9KsiAV7mxiCvJ@cluster0.3nv2u.mongodb.net/?retryWrites=true&w=majority"
+}
+module.exports = { config }
