bf93250036a296d52219071b9930309eb074bdc2
Jaime Andree Halire Huaman <49374710+jmhalire@users.noreply.github.com>
2023-03-19 17:48:11 -0500

    actualizando url mongodb

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 const config = {
     JWTSECRET: "mysecret",
-    MONGODB_URI: "mongodb+srv://jaimehalire:17e9KsiAV7mxiCvJ@cluster0.3nv2u.mongodb.net/?retryWrites=true&w=majority"
+    MONGODB_URI: "mongodb+srv://jaimehalire:17e9KsiAV7mxiCvJ@cluster0.3nv2u.mongodb.net/dbmultimedia?retryWrites=true&w=majority"
 }
 module.exports = { config }
